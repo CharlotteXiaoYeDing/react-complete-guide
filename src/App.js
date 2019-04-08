a21e2b8e9fba9feccb53afacd14e92d87239679a
Charlotte <charlottexiaoyeding@gmail.com>
2019-04-07 21:17:59 -0400

    Manipulating the State

--- src/App.js
+++ src/App.js
@@ -10,22 +10,34 @@ import Person from './Person/Person'
 class App extends Component {
   //only available if extends Component
   //this refers to the class
-  //state if changed, will re render the DOM
+  //if state/props changed, will re render the DOM
   state = {
       persons: [
         {name: "Max", age: 28 },
         {name: "Manu", age: 26 },
         {name: "Stephanie", age: 29}
-      ]
+      ],
+      otherState: 'some other value'
+  }
+
+  //use handler to indicate that it's a function not being called
+  //do not add parantheses when calling the function
+  switchNameHandler = () => {
+    //Don't do this: this.state.person[0].name = 'Maxi'
+    this.setState({persons: [
+      {name: "Maximilian", age: 28 },
+      {name: "Manu", age: 26 },
+      {name: "Stephanie", age: 27}
+    ]})
   }
   render() {
-//Every component must return some HTML to the DOM
+    //Every component must return some HTML to the DOM
     return (
       //class is a reserved word. Use className instead.
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbie: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
